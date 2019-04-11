2bd82e10d1b1a5295cc34724d2435952007df340
Niek <niekvandenbos@hotmail.com>
2019-04-11 20:47:45 +0200

    Update the app and app routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: ''          , component: LoginComponent     },
+  { path: 'dashboard' , component: DashBoardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashBoardComponent
   ],
   imports: [
     BrowserModule,
